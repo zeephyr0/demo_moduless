d461a4e24e6e0c57d23a9c766465a150bb286019
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-08-11 23:52:15 +0500

    Update AutoRF.py

--- AutoRF.py
+++ AutoRF.py
@@ -1,1 +1,13 @@
-577009581
+
+#и хули ты тут забыл?
+
+from .. import loader
+from asyncio import sleep
+
+@loader.tds 
+class AutoCWMod(loader.Module): 
+    """Нахуя тебе это?""" 
+    strings = {"name": "AutoCW"}
+
+    async def watcher(self, message):
+        if message.sender_id == 577009581:
