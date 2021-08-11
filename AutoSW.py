f91158927e0c5fa5c66a2435c02c5ffcefde731f
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-08-11 17:50:41 +0500

    Update AutoSW.py

--- AutoSW.py
+++ AutoSW.py
@@ -13,13 +13,13 @@ class AutoSWMod(loader.Module):
             if "Продолжить 💻Работать" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@StartupWarsBot', '/job');
-            if "Тебе не хватает Мотивации." in message.raw_text:
+            elif "Тебе не хватает Мотивации." in message.raw_text:
                 await sleep(3601);
                 await message.client.send_message('@StartupWarsBot', '/job');
-            if "Поздравляю! Твоя 🔥Мотивация полностью восстановлена" in message.raw_text:
+            elif "Поздравляю! Твоя 🔥Мотивация полностью восстановлена" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@StartupWarsBot', '/job');
-            if "Ты достиг нового уровня." in message.raw_text:
+            elif "Ты достиг нового уровня." in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@StartupWarsBot', '/levelup');
                 await sleep(2);
