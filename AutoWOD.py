045b17d2f213c54251b2337e84b543b11012042c
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-08-13 17:49:09 +0500

    Create AutoWOD.py

--- AutoWOD.py
+++ AutoWOD.py
@@ -13,3 +13,17 @@ class AutoWODMod(loader.Module):
         if message.sender_id == 913530224:
             if "В этой битве тебе не было равных, это безупречная победа!" in message.raw_text:
                 await sleep(3);
+                await message.client.send_message('@WorldDogs_bot', '⛩ Городские ворота');
+            if "Ты у городских ворот. Приключения ждут!" in message.raw_text:
+                await sleep(3);
+                await message.client.send_message('@WorldDogs_bot', '⚔️ PvP Арены');
+            if "⚔️ PVP Арены - это отличная возможность для каждого пса продемонстрировать свою силу, доблесть и отвагу. Сражайтесь с другими собаками в режимах 1х1, 3х3, принимайте участие в регулярных PvP турнирах и просто расслабляйтесь в авто-боях!" in message.raw_text:
+                await sleep(3);
+                await message.client.send_message('@WorldDogs_bot', '⚔️ Авто-Арена');
+            if "Плата за участие:" in message.raw_text:
+                await sleep(1);
+                await message.click(1);
+            if "Начать поиск соперника?" in message.raw_text:
+                await sleep(3);
+                await message.click();
+
