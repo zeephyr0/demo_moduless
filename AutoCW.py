52dcf55bef6c72c2dbb11db2e71f624076c02c51
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-11-30 12:38:49 +0500

    Update AutoCW.py

--- AutoCW.py
+++ AutoCW.py
@@ -19,13 +19,6 @@ class AutoCWMod(loader.Module):
                 await message.client.send_message('@citywars2_bot', '/buy_set_1');
                 await sleep(300);
                 await message.click();
-            elif "🎖MVP битвы:" in message.raw_text:
-                await sleep(180);
-                await message.client.send_message('@citywars2_bot', '/war');
-                await sleep(280);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-                await sleep(2);
-                await message.client.send_message('@citywars2_bot', '🚑 Лечим');
         if message.sender_id == 1399565278:
             if "@CityWars2Reports" in message.raw_text:
                 await sleep(2);
@@ -36,24 +29,8 @@ class AutoCWMod(loader.Module):
                 await sleep(2);
                 await message.client.send_message('@citywars2_bot', '👮 Патрулируем');
             if "#патруль" in message.raw_text:
-                await sleep(40);
+                await sleep(10);
                 await message.forward_to(-1001222463353);
-                await sleep(570);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-                await sleep(2);
-                await message.client.send_message('@citywars2_bot', '👮 Патрулируем');
-            if "🚑 Cостоянию здоровья" in message.raw_text:
-                await sleep(15);                
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-                await sleep(1);
-                await message.client.send_message('@citywars2_bot', '🚑 Лечим');
-            if "🚑 К сожалению, тебе не хватило умения, чтобы вылечить" in message.raw_text:
-                await sleep(245);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-                await sleep(1);
-                await message.client.send_message('@citywars2_bot', '🚑 Лечим');
-                await sleep(1);
-                await message.client.send_message('@citywars2_bot', '🚑 Лечим');
             if "#лечка" in message.raw_text:
                 await sleep(40);
                 await message.forward_to(701686415);
@@ -61,16 +38,8 @@ class AutoCWMod(loader.Module):
                 await message.client.send_message('@citywars2_bot', '🕹 Действия');
                 await sleep(1);
                 await message.client.send_message('@citywars2_bot', '🚑 Лечим');
-            if "Пришлось отдать 💵" in message.raw_text:
-                await sleep(10);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-                await sleep(2);
-                await message.client.send_message('@citywars2_bot', '🏪 Грабим');
-            elif "#ограбление" in message.raw_text:
+            if "#ограбление" in message.raw_text:
                 await sleep(10)
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-                await sleep(2);
-                await message.client.send_message('@citywars2_bot', '🏪 Грабим');
                 await message.forward_to(701686415);
             if "🎈 У тебя есть неполученный бонус!" in message.raw_text:
                 await sleep(50);
