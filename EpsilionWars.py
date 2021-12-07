8f7145e7e09dfe0128fed8e519b5940e500427e2
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-12-08 01:56:21 +0500

    Update EpsilionWars.py

--- EpsilionWars.py
+++ EpsilionWars.py
@@ -13,25 +13,10 @@ class EpsillionWarsMod(loader.Module):
         if message.sender_id == 776510403:
             if "Куда будешь бить?" in message.raw_text:
                 await sleep(3)
-                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
-            if "Ход 1" in message.raw_text:
+                await message.client.send_message('@EpsilionWarBot', 'В ноги');
+            if "Ход" in message.raw_text:
                 await sleep(3)
-                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
-            if "Ход 2" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
-            if "Ход 3" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
-            if "Ход 4" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
-            if "Ход 5" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
-            if "Ход 6" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
+                await message.client.send_message('@EpsilionWarBot', 'В ноги');
             if "Что будешь блокировать?" in message.raw_text:
                 await sleep(3)
                 await message.client.send_message('@EpsilionWarBot', 'Ноги, голова');
