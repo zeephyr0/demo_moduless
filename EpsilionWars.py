82c6c2d8cbfae71b61bdf7df2084f11a1453c965
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-12-07 19:45:51 +0500

    Create EpsilionWars.py

---
+++ EpsilionWars.py
@@ -0,0 +1,19 @@
+#и хули ты тут забыл?
+
+from .. import loader
+from asyncio import sleep
+import random
+
+@loader.tds 
+class EpsillionWarsMod(loader.Module): 
+    """Нахуя тебе это?""" 
+    strings = {"name": "EpsillionWars"}
+
+    async def watcher(self, message):
+        if message.sender_id == 776510403:
+            if "Вот ты и встретил своего врага" in message.raw_text:
+                await sleep(3)
+                await message.client.send_message('@EpsilionWarsBot', 'В ноги');
+            if "Что будешь блокировать?" in message.raw_text:
+                await sleep(3)
+                await message.client.send_message('@EpsilionWarBot', 'Ноги, голова');
