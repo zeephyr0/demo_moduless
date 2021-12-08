d7b887620405e0b1fc6ef524b90c260b35a8ae32
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-12-08 10:29:33 +0500

    Update EpsilionWars.py

--- EpsilionWars.py
+++ EpsilionWars.py
@@ -10,24 +10,27 @@ class EpsillionWarsMod(loader.Module):
     strings = {"name": "EpsillionWars"}
 
     async def watcher(self, message):
+        time = [2, 5, 8]
+        attack = ['В ноги', 'В пояс', 'В живот', 'В грудь', 'В голову']
+        defense = ['Ноги, голова', 'Пояс, ноги', 'Живот, пояс', 'Грудь, живот', 'Голова, грудь']
         if message.sender_id == 776510403:
             if "Куда будешь бить?" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarBot', 'В ноги');
-            if "📍 Ты победил своего врага" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarBot', '✅ Забрать нaграду');
-            elif "Ход" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarBot', 'В ноги');
+                await sleep(random.choise(time))
+                await message.client.send_message('@EpsilionWarBot', random.choise(attack));   
+            if "Ход" in message.raw_text:
+                await sleep(random.choise(time))
+                await message.client.send_message('@EpsilionWarBot', random.choise(attack));   
             if "Что будешь блокировать?" in message.raw_text:
-                await sleep(3)
-                await message.client.send_message('@EpsilionWarBot', 'Ноги, голова');
+                await sleep(random.choise(time))
+                await message.client.send_message('@EpsilionWarBot', random.choise(defense));   
+            if "📍 Ты победил своего врага" in message.raw_text:
+                await sleep(random.choise(time))
+                await message.client.send_message('@EpsilionWarBot', '✅ Забрать нaграду');
             if "💖 Ваше здоровье полностью восстановлено" in message.raw_text:
-                await sleep(3)
+                await sleep(random.choise(time))
                 await message.client.send_message('@EpsilionWarBot', '⚔️ Найти врагов');
             if "Ты был отправлен восстанавливаться в город" in message.raw_text:
-                await sleep(3)
+                await sleep(random.choise(time))
                 await message.client.send_message('@EpsilionWarBot', '🗺 Карта');
-                await sleep(3)
+                await sleep(random.choise(time))
                 await message.client.send_message('@EpsilionWarBot', '🏕 Дом в лесу');
