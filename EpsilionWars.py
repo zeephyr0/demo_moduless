84929a3575004be09e63d50b5da4d7d09fd38cb0
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-12-12 20:21:08 +0500

    Update EpsilionWars.py

--- EpsilionWars.py
+++ EpsilionWars.py
@@ -12,7 +12,7 @@ class EpsillionWarsMod(loader.Module):
     async def watcher(self, message):
         time = [2, 4, 6]
         attack = ['В ноги', 'В пояс', 'В живот', 'В грудь', 'В голову']
-        defense = ['Ноги, голова, грудь', 'Пояс, ноги, голова', 'Живот, пояс, ноги', 'Голову, грудь, живот', 'Грудь, живот, пояс']
+        defense = ['Ноги, голова', 'Пояс, ноги', 'Живот, пояс', 'Грудь, живот', 'Голова, грудь']
         if message.sender_id == 776510403:
             if "Куда будешь бить?" in message.raw_text:
                 await sleep(random.choice(time))
