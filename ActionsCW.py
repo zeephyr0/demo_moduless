5c8b02e78cd29441d46d7dfec4c27dc40b09898b
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2021-12-18 14:28:27 +0500

    Update ActionsCW.py

--- ActionsCW.py
+++ ActionsCW.py
@@ -16,42 +16,35 @@ class ActionsCWMod(loader.Module):
         if message.sender_id == 1399565278:
             if "👮 Ты отдохнул" in message.raw_text:
                 await sleep(10);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
                 await sleep(2);
-                await message.client.send_message('@citywars2_bot', '👮 Патрулируем');
+                await message.client.send_message('@CityWars2_bot', '👮 Патрулируем');
             elif "👮 На улицах" in message.raw_text:
                 await sleep(10);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
                 await sleep(2);
-                await message.client.send_message('@citywars2_bot', '👮 Патрулируем');
+                await message.client.send_message('@CityWars2_bot', '👮 Патрулируем');
             if "#патруль" in message.raw_text:
                 await sleep(40);
                 await message.forward_to(-1001222463353);
             if "🚑 Cостоянию здоровья" in message.raw_text:
                 await sleep(15);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-            if "🚑 К сожалению, тебе не хватило умения, чтобы вылечить" in message.raw_text:
-                await sleep(245);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
+            if "🚑 Ты отдохнул" in message.raw_text:
+                await sleep(5);
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
                 await sleep(1);
-                await message.client.send_message('@citywars2_bot', '🚑 Лечим');
-            if "#лечка" in message.raw_text:
-                await sleep(40);
-                await message.forward_to(701686415);
-                await sleep(205);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
-                await sleep(1);
-                await message.client.send_message('@citywars2_bot', '🚑 Лечим');
+                await message.client.send_message('@CityWars2_bot', '🚑 Лечим');
             if "Пришлось отдать 💵" in message.raw_text:
                 await sleep(10);
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
                 await sleep(2);
-                await message.client.send_message('@citywars2_bot', '🏪 Грабим');
+                await message.client.send_message('@CityWars2_bot', '🏪 Грабим');
             elif "#ограбление" in message.raw_text:
                 await sleep(10)
-                await message.client.send_message('@citywars2_bot', '🕹 Действия');
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
                 await sleep(2);
-                await message.client.send_message('@citywars2_bot', '🏪 Грабим');
+                await message.client.send_message('@CityWars2_bot', '🏪 Грабим');
                 await message.forward_to(701686415);
             if "Глава твоей банды запускает атаку на" in message.raw_text:
                 await sleep(random.choice(time2));
