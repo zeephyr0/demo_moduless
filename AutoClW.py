42cbca0355946fa6c65e436474ab67cbcd0e4aab
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-03-03 21:58:26 +0500

    Create AutoClW.py

---
+++ AutoClW.py
@@ -0,0 +1,25 @@
+#и хули ты тут забыл?
+#создано в развлекательных и ознакомительных целях
+#by ZEPHYR0
+
+from .. import loader
+from asyncio import sleep
+
+@loader.tds 
+class AutoClWMod(loader.Module): 
+    """<b>Модуль для частичной автоматизации игры</b> @clan_warsbot\n\n<b>Особенности модуля:</b>\n<code>• Ходить в приключения</code>\nОстальное будет добавляться постепенно по ходу освоения игры автором\n\n<b>❗Модуль создан в ознакомительных целях и может работать нестабильно</b>""" 
+    strings = {"name": "AutoClW"}
+
+    async def watcher(self, message):
+        if message.chat_id == -1001222463353 and message.sender_id == 701686415:
+            if "🎊Приключение «Кража люков» успешно завершено!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🗺Приключения');
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '👻 Кража люков (15 минут)');
+            if "🚓Приключение провалено!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🗺Приключения');
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '👻 Кража люков (15 минут)');
+
