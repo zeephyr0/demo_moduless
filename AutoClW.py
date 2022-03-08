f64c34c2fcdb1cf7dca07cd9d36dc760d78908c0
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-03-08 15:32:16 +0500

    Update AutoClW.py

--- AutoClW.py
+++ AutoClW.py
@@ -1,67 +1,37 @@
 #и хули ты тут забыл?
 #создано в развлекательных и ознакомительных целях
 #by ZEPHYR0
-#v1.3.0
+#v1.4.1
 
 from .. import loader
 from asyncio import sleep
 
 @loader.tds 
 class AutoClWMod(loader.Module): 
-    """Модуль для частичной автоматизации игры @clan_warsbot\n\nОсобенности модуля:\n• Автоматически ходить в приключение "Кража люков"\n• Возможность отправить в путешествие "Кража люков" командой adv\n• Возможность отменить путешествие командой stopadv\n• Возможность отправить персонажа на войну командой\nОстальное будет добавляться постепенно по ходу освоения игры автором\n\n❗Модуль создан в ознакомительных целях и может работать нестабильно\nv1.3.0""" 
+    """Модуль для частичной автоматизации игры @clan_warsbot\n\nОсобенности модуля:\n• Автоматически ходить в приключение "Кража люков"\nОстальное будет добавляться постепенно по ходу освоения игры автором\n\n❗Модуль создан в ознакомительных целях и может работать нестабильно\nv1.3.0""" 
     strings = {"name": "AutoClW"}
-
-    async def attack1(self, message):
-      #атака на РЖД🚂
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Отменить приключение❌');
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'РЖД🚂');
-
-    async def attack2(self, message):
-      #атака на Аэропорт🛩
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Отменить приключение❌');
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Аэропорт🛩');
-
-    async def attack3(self, message):
-      #атака на Таксопарк🚕
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Отменить приключение❌');
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Таксопарк🚕');
-
-    async def attack4(self, message):
-      #атака на Заправочную станцию⛽️
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Отменить приключение❌');
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Заправочная станция⛽️');
-
-    async def attack5(self, message):
-      #атака на Порт🚢
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Отменить приключение❌');
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Порт🚢');
-
-    async def adv(self, message):
-      #начать приключение
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', '🗺Приключения');
-
-    async def stopadv(self, message):
-      #закончить приключение
-        await sleep(2);
-        await message.client.send_message('@clan_warsbot', 'Отменить приключение❌');
 
     async def watcher(self, message):
         if message.sender_id == 2063668248:
+            if "🏦🔥Банк ограблен!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🏦 Ограбление банка (1 час)');
+            if "🚓Кто-то из вас оказался крысой и слил ваши планы полиции!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🗺Приключения');
+            if "🎫Осталось билетов: 1" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🔍Найти подельников');
+            elif "🎫Осталось билетов: 2" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🔍Найти подельников');
+            elif "🎫Осталось билетов: 3" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🔍Найти подельников');
             if "🎊Приключение «Кража люков» успешно завершено!" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@clan_warsbot', '🗺Приключения');
-            if "🚓Приключение провалено!" in message.raw_text:
+            elif "🚓Приключение провалено!" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@clan_warsbot', '🗺Приключения');
             if "Отправиться в приключение💎" in message.raw_text:
