5655a2b024e1cae953491f404acc6b2b89f09158
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-03-11 07:47:31 +0500

    Update AutoBS.py

--- AutoBS.py
+++ AutoBS.py
@@ -7,6 +7,22 @@ class AutoBSMod(loader.Module):
 
     async def watcher(self, message):
         if message.sender_id == 2063668248:
+            if "🎊Приключение «Кража люков» успешно завершено!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@BSv2Bot', '⚔️ Гарнизон');
+                await sleep(2);
+                await message.client.send_message('@BSv2Bot', '🗺 Разведка');
+            elif "🚓Приключение провалено!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@BSv2Bot', '⚔️ Гарнизон');
+                await sleep(2);
+                await message.client.send_message('@BSv2Bot', '🗺 Разведка');
+        if message.sender_id == 764095451:
+            if "🗺 Разведка" in message.raw_text:
+                await sleep(2);
+                await message.click();
+                await sleep(2);
+                await message.click(1);
             if "🏦🔥Банк ограблен!" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@clan_warsbot', '🏦 Ограбление банка (1 час)');
