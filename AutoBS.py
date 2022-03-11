be831293fb5e02d8d8d9b0534113786d76bbe5db
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-03-11 07:40:40 +0500

    Update AutoBS.py

--- AutoBS.py
+++ AutoBS.py
@@ -1,1 +1,12 @@
-.
+from .. import loader
+from asyncio import sleep
+
+@loader.tds 
+class AutoBSMod(loader.Module): 
+    strings = {"name": "AutoBS"}
+
+    async def watcher(self, message):
+        if message.sender_id == 2063668248:
+            if "🏦🔥Банк ограблен!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🏦 Ограбление банка (1 час)');
