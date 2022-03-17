4e9e6d3817860855dacaf92ea6def37d5044fbf5
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-03-17 07:53:31 +0500

    Update AutoBsClW.py

--- AutoBsClW.py
+++ AutoBsClW.py
@@ -21,6 +21,28 @@ class AutoBSMod(loader.Module):
                 await message.client.send_message('@BSv2Bot', '⚔️ Гарнизон');
                 await sleep(2);
                 await message.client.send_message('@BSv2Bot', '🗺 Разведка');
+            if "🏦🔥Банк ограблен!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🏦 Ограбление банка (1 час)');
+            if "🚓Кто-то из вас оказался крысой и слил ваши планы полиции!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🗺Приключения');
+            if "🎫Осталось билетов: 1" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🔍Найти подельников');
+            elif "🎫Осталось билетов: 2" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🔍Найти подельников');
+            elif "🎫Осталось билетов: 3" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🔍Найти подельников');
+            if "Отправиться в приключение💎" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '👻 Кража люков (15 минут)');
+        if message.chat_id == -1001649471126 and message.sender_id == 2063668248:
+            if "🎭Результаты войны кланов за предприятия" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '🗺Приключения');
         if message.sender_id == 764095451:
             if "[🐉🤺]ZEPHYR" in message.raw_text:
                 await sleep(2);
@@ -99,6 +121,8 @@ class AutoBSMod(loader.Module):
                 await sleep(2);
                 await message.client.send_message('@BSv2Bot', '🗺 Разведка');
             if "Всего раундов:" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '👻 Кража люков (15 минут)');
                 await sleep(2);
                 await message.forward_to(666473433);
             if "Копать еще:" in message.raw_text:
