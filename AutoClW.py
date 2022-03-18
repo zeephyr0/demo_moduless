7c14fb6c7788d3686d602b5630fca50eb74b0519
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-03-19 01:24:56 +0500

    Update AutoClW.py

--- AutoClW.py
+++ AutoClW.py
@@ -1,24 +1,28 @@
 #и хули ты тут забыл?
 #создано в развлекательных и ознакомительных целях
 #by ZEPHYR0
-#v1.5.1
 
 from .. import loader
 from asyncio import sleep
 
 @loader.tds 
 class AutoClWMod(loader.Module): 
-    """Модуль для частичной автоматизации игры @clan_warsbot\n\nОсобенности модуля:\n• Автоматически ходить в приключение "Кража люков"\nОстальное будет добавляться постепенно по ходу освоения игры автором\n\n❗Модуль создан в ознакомительных целях и может работать нестабильно\nv1.3.0""" 
     strings = {"name": "AutoClW"}
 
     async def watcher(self, message):
         if message.sender_id == 2063668248:
+            if "🎊Приключение «Кража люков» успешно завершено!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '👻 Кража люков (15 минут)');
+            elif "🚓Приключение провалено!" in message.raw_text:
+                await sleep(2);
+                await message.client.send_message('@clan_warsbot', '👻 Кража люков (15 минут)');
             if "🏦🔥Банк ограблен!" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@clan_warsbot', '🏦 Ограбление банка (1 час)');
             if "🚓Кто-то из вас оказался крысой и слил ваши планы полиции!" in message.raw_text:
                 await sleep(2);
-                await message.client.send_message('@clan_warsbot', '🗺Приключения');
+                await message.client.send_message('@clan_warsbot', '🏦 Ограбление банка (1 час)');
             if "🎫Осталось билетов: 1" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@clan_warsbot', '🔍Найти подельников');
@@ -35,10 +39,4 @@ class AutoClWMod(loader.Module):
             if "🎭Результаты войны кланов за предприятия" in message.raw_text:
                 await sleep(2);
                 await message.client.send_message('@clan_warsbot', '🗺Приключения');
-        if message.sender_id == 764095451:
-            if "Всего раундов:" in message.raw_text:
-                await sleep(2);
-                await message.client.send_message('@clan_warsbot', '👻 Кража люков (15 минут)');
-
-
 
