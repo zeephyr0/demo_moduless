285e8a641f23c9cd8a9f07cec12ff0a109c13376
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-04-17 23:18:23 +0500

    Update ClWActions.py

--- ClWActions.py
+++ ClWActions.py
@@ -11,6 +11,7 @@ class ClWActionsMod(loader.Module):
 
     async def watcher(self, message):
         if message.group_id and message.sender_id == 920762514:
-            if "" in message.raw_text:
-                await sleep(2);
-                await message.client.send_message('@clan_warsbot', '');
+            if "⚔️Сразитесь против других игроков из враждующих кланов!" in message.raw_text:
+                await sleep(1);
+                await message.client.send_message('@clan_warsbot', '⚔️Арена');
+  
