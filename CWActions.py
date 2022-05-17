862f251f03a51e14ab3ea0c5edcd3b077ed90d3f
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-05-17 15:06:21 +0500

    Create CWActions.py

---
+++ CWActions.py
@@ -0,0 +1,40 @@
+#и хули ты тут забыл?
+
+from .. import loader
+from asyncio import sleep
+import random
+
+@loader.tds 
+class ActionsCWMod(loader.Module): 
+    """Нахуя тебе это?""" 
+    strings = {"name": "ActionsCW"}
+
+    async def watcher(self, message):
+        time1 = [4, 120]
+        buttons = [0, 1, 2]
+        time2 = [2, 4, 6]
+        if message.sender_id == 1399565278:
+            if "👮 Ты отдохнул" in message.raw_text:
+                await sleep(random.choise(time2));
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
+                await sleep(2);
+                await message.client.send_message('@CityWars2_bot', '👮 Патрулируем');
+            elif "👮 На улицах" in message.raw_text:
+                await sleep(random.choise(time2));
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
+                await sleep(2);
+                await message.client.send_message('@CityWars2_bot', '👮 Патрулируем');
+            if "🚑 Cостоянию здоровья" in message.raw_text:
+                await sleep(random.choise(time2));
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
+                await sleep(1);
+                await message.client.send_message('@CityWars2_bot', '🚑 Лечим');
+            if "🚑 Ты отдохнул" in message.raw_text:
+                await sleep(random.choise(time2));
+                await message.client.send_message('@CityWars2_bot', '🕹 Действия');
+                await sleep(1);
+                await message.client.send_message('@CityWars2_bot', '🚑 Лечим');
+        if message.sender_id == 1159155249:
+            if "У меня для вас есть несколько 🎁 Подарков!" in message.raw_text:
+                await sleep(
+                await message.click(random.choise(time2));
