a0fcf8dcc5abda6787fbb408b5a3060498e744ff
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-06-06 19:39:19 +0500

    Update AutomaticCW.py

--- AutomaticCW.py
+++ AutomaticCW.py
@@ -1,3 +1,6 @@
+#by @ZEPHYR0 (просто спиздил модуль)
+#спиздил во благо
+
 import random 
 from .. import loader, utils 
 from datetime import timedelta 
@@ -6,7 +9,7 @@ from telethon.tl.types import Message
  
 @loader.tds 
 class AutoCWMod(loader.Module): 
- """"" 
+ """ты лох""" 
  strings = { 
   'name': 'AutoCW', 
   'cwmon': '<code>✅ Автоматизация CityWars включена</code>', 
@@ -25,21 +28,21 @@ class AutoCWMod(loader.Module):
   self.city = 5505560402
 
  async def cwoncmd(self, message): 
-  """""" 
+  """Включить автоматизацию""" 
   status = self.db.get(self.name, "status", False) 
   if status: return await message.edit(self.strings['cwon_already']) 
   self.db.set(self.name, "status", True)
   await message.edit(self.strings['cwon'])
 
  async def cwoffcmd(self, message): 
-  """""" 
+  """Выключить автоматизацию, лол""" 
   self.db.set(self.name, 'status', False) 
   money = self.db.get(self.name, 'money', 0) 
   if money: self.db.set(self.name, 'money', 0) 
   await message.edit(self.strings['cwoff'].replace("%money%", str(money)))
 
  async def balancecmd(self, message): 
-  """""" 
+  """Проверить сколько нафармил""" 
   money = self.db.get(self.name, "money", 0) 
   await message.edit(self.strings['balance'].replace("%money%", str(money))) 
 
