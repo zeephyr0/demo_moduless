5f2b3bd02fe614199f690d72dc0d401f61ef240a
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-09-03 21:33:21 +0500

    Update ByOwl.py

--- ByOwl.py
+++ ByOwl.py
@@ -11,6 +11,9 @@ class ByOwlMod(loader.Module):
  strings = {"name": "ByOwl"}
 
  colors = ['10000 к', '10000 ч']
+ retur
  async def watcher(self, message): 
-  if message.sender_id == 5505560402:
-   if 
+  if message.chat_id == -1001163341690 and message.sender_id == 5143234239:
+   if "💰🏪 Найм рабочих/Постройки/Акции" in message.raw_text:
+    await sleep(2);
+    await message.client.send_message('@idle_city_bot', 'Построить Электростанцию 🏗⚡️');
