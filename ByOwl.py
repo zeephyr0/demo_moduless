301dc233a754ef8a265e44191842d58a269568d2
ZEPHYR <88072383+zeephyr0@users.noreply.github.com>
2022-09-08 13:41:02 +0500

    Update ByOwl.py

--- ByOwl.py
+++ ByOwl.py
@@ -47,56 +47,11 @@ class ByOwlMod(loader.Module):
 
  async def watcher(self, message): 
   if message.chat_id == -1001163341690 and message.sender_id == 5143234239:
-   if "HDR ставка 100 выиграл 200 на красное" in message.raw_text:
+   if "Рулетка" in message.raw_text:
     await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 200 выиграл 400 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 400 выиграл 800 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 1600 выиграл 3200 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 3200 выиграл 6400 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 6400 выиграл 12800 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 12800 выиграл 23600 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 23600 выиграл 45200 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 45200 выиграл 90400 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 90400 выиграл 180800 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 180800 выиграл 361600 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 361600 выиграл 723200 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 723200 выиграл 1446400 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 1446400 выиграл 2892800 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "HDR ставка 2892800 выиграл 5785600 на красное" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['red']);
-   elif "Рулетка" and "⚫️" in message.raw_text:
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['repeat']);
-    await sleep(1);
-    await message.client.send_message(self.owl_group, self.strings['double']);
+    await message.client.send_message(self.owl_group, 'повторить');
+    await sleep(12);
+    await message.client.send_message(self.owl_group, 'го');
 
 
 
