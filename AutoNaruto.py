6b362d7e95183fce48d4ccfd38422d03013f12a9
ZEPHYR <democrat18906@gmail.com>
2024-08-16 00:03:05 +0500

    Update AutoNaruto.py

--- AutoNaruto.py
+++ AutoNaruto.py
@@ -54,7 +54,7 @@ class NarutoAdventureMod(loader.Module):
             if "В одном из городов, где вы остановились, вы нашли онсэн. Абонемент стоит 2 млн рё." in message.raw_text:
                if message.reply_markup:
                    if message.reply_markup.rows:  # Проверяем, есть ли строки кнопок
-                       if len(message.reply_markup.rows) > 0 and len(message.reply_markup.rows[0].buttons) >= 2:  # Проверяем, что есть минимум 4 кнопки
+                       if len(message.reply_markup.rows) > 0 and len(message.reply_markup.rows[0].buttons) >= 3:  # Проверяем, что есть минимум 4 кнопки
                 # Получаем текст четвёртой кнопки и отправляем его
                            button_text = message.reply_markup.rows[0].buttons[1].text  # Индекс 3 для четвёртой кнопки
                            await asyncio.sleep(random.uniform(2, 7))
